d56b74b305790e0b4aa1f03aa4a2813bd460c711
ismael20200611 <ismael20200611@gmail.com>
2026-01-09 17:11:44 +0300

    update translate

--- js/script.js
+++ js/script.js
@@ -8,7 +8,35 @@
 //Translate
 function googleTranslateElementInit() {
   new google.translate.TranslateElement(
-    { pageLanguage: 'en' },
+    {
+      pageLanguage: 'en',
+      autoDisplay: false
+    },
     'google_translate_element'
   );
+
+  // Apply saved language
+  var lang = getCookie('googtrans');
+  if (lang) {
+    var select = document.querySelector('.goog-te-combo');
+    if (select) {
+      select.value = lang.split('/')[2];
+      select.dispatchEvent(new Event('change'));
+    }
+  }
 }
+
+// Save language when user changes it
+document.addEventListener('change', function (e) {
+  if (e.target.classList.contains('goog-te-combo')) {
+    var lang = e.target.value;
+    document.cookie = 'googtrans=/en/' + lang + ';path=/';
+  }
+});
+
+// Cookie helper
+function getCookie(name) {
+  var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? match[2] : null;
+}
+// finish translate method
